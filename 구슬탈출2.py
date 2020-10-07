9c730dcaa96e68a548b31e85a7b82bca615cbe06
박종한 <exuberant94@naver.com>
2020-10-08 03:05:01 +0900

    201008 done2

--- 구슬탈출2.py
+++ 구슬탈출2.py
@@ -1,137 +1,73 @@
 from collections import deque
 n, m = map(int, input().split())
 arr = [list(input()) for _ in range(n)]
-dx, dy = (-1, 1, 0, 0), (0, 0, -1, 1)
-
-
-# 빨간 구슬 굴리기
-# x 세로
-# y 가로
-# d 방향 (상하좌우 순)
-def roll_red(x, y, d):
-    nx, ny = x, y
-    while True:
-        if arr[nx + dx[d]][ny + dy[d]] != '.':
-            break
-        nx += dx[d]
-        ny += dy[d]
-    return nx, ny
-
-
-# 파란 구슬 굴리기 (이하 생략)
-def roll_blue(x, y, d):
-    nx, ny = x, y
-    while True:
-        if arr[nx + dx[d]][ny + dy[d]] != '.':
-            break
-        nx += dx[d]
-        ny += dy[d]
-    return nx, ny
-
-
-rx, ry = -1, -1 # 빨간 구슬 좌표
-bx, by = -1, -1 # 파란 구슬 좌표
+dxs, dys = (-1, 1, 0, 0), (0, 0, -1, 1)
+rex, rey = -1, -1 # 빨간 구슬 좌표
+blx, bly = -1, -1 # 파란 구슬 좌표
 
 # 구슬 좌표 찾기
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 'R':
-            rx, ry = i, j
+            rex, rey = i, j
             arr[i][j] = '.'
         elif arr[i][j] == 'B':
-            bx, by = i, j
+            blx, bly = i, j
             arr[i][j] = '.'
 
 
 def bfs(r_x, r_y, b_x, b_y):
     q = deque()
-    q.append((0, r_x, r_y, b_x, b_y))
+    q.append((1, r_x, r_y, b_x, b_y))
 
     while q:
         cnt, red_x, red_y, blue_x, blue_y = q.popleft()
-        if cnt >= 10:
+        if cnt > 10:
             break
-        for dr in range(4):
-            if not dr:
-                if red_x < blue_x: # 빨간 구슬 먼저 위로 구름
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                    arr[nrx][nry] = 'R'
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                else:
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                    arr[nbx][nby] = 'B'
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                arr[nrx][nry], arr[nbx][nby] = '.', '.'
-                if arr[nrx-1][nry] == 'O': # 빨간 구슬이 나가는 경우
-                    if nry == nby and nrx + 1 == nbx:
-                        # 파란 구슬도 빠져나간다면 바로 밑에 있을 것임 => 큐에 삽입하지 않는다.
-                        continue
-                    else:
-                        print(cnt+1)
-                        return
-                elif arr[nbx-1][nby] == 'O':
-                    continue
-            elif dr == 1:
-                if red_x > blue_x: # 빨간 구슬 먼저 아래로 구름
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                    arr[nrx][nry] = 'R'
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                else:
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                    arr[nbx][nby] = 'B'
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                arr[nrx][nry], arr[nbx][nby] = '.', '.'
-                if arr[nrx+1][nry] == 'O': # 빨간 구슬이 나가는 경우
-                    if nry == nby and nrx - 1 == nbx:
-                        # 파란 구슬도 빠져나간다면 바로 위에 있을 것임 => 큐에 삽입하지 않는다.
-                        continue
-                    else:
-                        print(cnt+1)
-                        return
-                elif arr[nbx+1][nby] == 'O':
-                    continue
-            elif dr == 2:
-                if red_y < blue_y: # 빨간 구슬 먼저 좌로 구름
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                    arr[nrx][nry] = 'R'
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                else:
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                    arr[nbx][nby] = 'B'
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                arr[nrx][nry], arr[nbx][nby] = '.', '.'
-                if arr[nrx][nry-1] == 'O': # 빨간 구슬이 나가는 경우
-                    if nrx == nbx and nry+1 == nby:
-                        # 파란 구슬도 빠져나간다면 바로 오른쪽에 있을 것임 => 큐에 삽입하지 않는다.
-                        continue
-                    else:
-                        print(cnt+1)
-                        return
-                elif arr[nbx][nby-1] == 'O':
-                    continue
-            else:
-                if red_y > blue_y: # 빨간 구슬 먼저 우로 구름
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                    arr[nrx][nry] = 'R'
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                else:
-                    nbx, nby = roll_blue(blue_x, blue_y, dr)
-                    arr[nbx][nby] = 'B'
-                    nrx, nry = roll_red(red_x, red_y, dr)
-                arr[nrx][nry], arr[nbx][nby] = '.', '.'
-                if arr[nrx][nry+1] == 'O': # 빨간 구슬이 나가는 경우
-                    if nrx == nbx and nry-1 == nby:
-                        # 파란 구슬도 빠져나간다면 바로 왼쪽에 있을 것임 => 큐에 삽입하지 않는다.
-                        continue
-                    else:
-                        print(cnt+1)
-                        return
-                elif arr[nbx][nby+1] == 'O':
-                    continue
-            if red_x == nrx and red_y == nry and blue_x == nbx and blue_y == nby:
+        for dx, dy in zip(dxs, dys):
+            b_out, r_out = False, False
+            rx, ry = red_x, red_y
+            bx, by = blue_x, blue_y
+            flag = False
+            while True:
+                rx += dx
+                ry += dy
+                if arr[rx][ry] == 'O':
+                    r_out = True  # 빨간 구슬 아웃
+                    break
+                elif arr[rx][ry] == '#':
+                    rx -= dx
+                    ry -= dy
+                    break
+                if [rx, ry] == [bx, by]:  # 이 말은 무조건 파란 구슬이 먼저 움직인단 소리
+                    flag = True
+                    rx -= dx
+                    ry -= dy
+                    break
+            while True:
+                bx += dx
+                by += dy
+                if arr[bx][by] == 'O':  # 기울인 곳에 빨간 구슬은 일단 없음
+                    b_out = True  # 파란 구슬 아웃
+                    break
+                elif arr[bx][by] == '#':
+                    bx -= dx
+                    by -= dy
+                    if flag:
+                        rx = bx - dx
+                        ry = by - dy
+                    break
+                if [bx, by] == [rx, ry]:  # 빨간 구슬이 먼저 움직임
+                    bx -= dx
+                    by -= dy
+                    break
+            if b_out or [red_x, red_y] == [rx, ry] and [blue_x, blue_y] == [bx, by]:
                 continue
-            q.append((cnt+1, nrx, nry, nbx, nby))
+            if r_out:
+                print(cnt)
+                return
+            q.append((cnt+1, rx, ry, bx, by))
     print(-1)
 
 
-bfs(rx, ry, bx, by)
+bfs(rex, rey, blx, bly)
